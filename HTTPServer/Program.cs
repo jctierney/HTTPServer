49170e0d7bd49fe27d0dc8bb05fa5753c7dac2ca
Jordon Biondo <biondoj@mail.gvsu.edu>
2013-02-06 03:24:45 -0500

    Test TcpListener

--- HTTPServer/Program.cs
+++ HTTPServer/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 
 namespace HTTPServer
 {
@@ -9,6 +12,22 @@ namespace HTTPServer
     {
         static void Main(string[] args)
         {
+			TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+			listener.Start();
+			TcpClient client = listener.AcceptTcpClient();
+			StreamReader reader = new StreamReader(client.GetStream());
+			StreamWriter writer = new StreamWriter(client.GetStream());
+			String x = reader.ReadLine();
+			int i = 1;
+			while(!String.IsNullOrEmpty(x)) {
+				Console.WriteLine(i.ToString()+ x);
+				x = reader.ReadLine();
+				i++;
+
+			}
+
+			writer.WriteLine("HTTP/1.0 404 Not Found\r\n");
+
         }
     }
 }
